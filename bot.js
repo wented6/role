17f12b6180dda2b02a3a3cb26e88e8d0beff1708
wented6 <44713416+wented6@users.noreply.github.com>
2018-12-24 12:35:52 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
 
 
 client.on('guildMemberAdd', member=> {
-    member.addRole(member.guild.roles.find("name","اسم الرتبه"));
+    member.addRole(member.guild.roles.find("name","Uni."));
     });
 
 
